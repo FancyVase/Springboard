04014b36bfae25ea1f790c27a7fab5fc718e8c28
Jessica Noss <jmn@mit.edu>
2016-04-12 14:14:01 -0400

    importing all dive data from csv

--- js/make_divelist.js
+++ js/make_divelist.js
@@ -1,14 +1,14 @@
 // JS functions for the make_divelist.html page
 
+function loadDiveData(filename) {
+    var diveData = getDivesFromCsv(filename);
+    populateDivelist(diveData);
+}
 
-
-
-$(document).ready(function(){
-
-    // Get the dives list from the csv file
-    // Cols are: ID, name, DD
-    var client = new XMLHttpRequest();
-    client.open('GET', './dives.csv', false);
+function getDivesFromCsv(filename) {
+    // all_dives.csv cols are: ID, name, DD
+    var client = new XMLHttpRequest(); //todo see console warning
+    client.open('GET', filename, false);
     client.send();
     var my_csv = client.responseText;
     my_csv = my_csv.split("\n");
@@ -16,30 +16,51 @@ $(document).ready(function(){
     for (var i=0;i<my_csv.length;i++){
         dives_set.push(my_csv[i].split(","));
     }
-    
-    // Add the dives on the list to the UI
-    for (var i=0;i<dives_set.length;i++){
-        $("#dive-database-table").append('<tr class="dive-entry" id="'+dives_set[i][0]+'"><td class="dive-id">'+dives_set[i][0]+'</td><td class="dive-name">'+dives_set[i][1]+'</td><td class="dive-experience">Still learning</td><td class="dive-predicted-score">42.00</td><td class="dive-high-score">42.00</td><td class="dive-average-score">42.00</td><td class="dive-last-performed">1/07/2016</td><td class="dive-selector"><a class="selection-circle">&nbsp;</a></td></tr>');
-    }
-    
-    
-    // Action Listeners
-    $(".dive-entry").click(function() {
-        toggleDive(this);
-    });
+    return dives_set;
+}
 
-    // Listener Functions
-    function toggleDive(clicked) {
-        console.log("toggling!");
-        $("#divelist-container").addClass("hide-quicklist");
-        $(clicked).toggleClass("selected");
-        if ($(clicked).hasClass("selected")) { // Add it to the box
-            $("#list-view").append('<span class="selected-dive" id="'+clicked.id+'_selected">'+clicked.id+' &nbsp; <strong>Dive</strong></span>');
-        } else { // remove it from the box
-            $('#'+clicked.id+'_selected').remove();
+function populateDivelist(diveData) {
+    var diveSelector = '<a class="selection-circle"></a>';
+    for (var i=0; i<diveData.length; i++) {
+        var $newDiveRow = $("<tr></tr>", {"class":"dive-entry", "id":diveData[i][0]});
+        var diveProperties = {
+            "dive-id": diveData[i][0],
+            "dive-name": diveData[i][1],
+//            "dive-dd": diveData[i][2], //for all_dives.csv
+            "dive-experience": diveData[i][2],
+            "dive-predicted-score": diveData[i][3],
+            "dive-high-score": diveData[i][4],
+            "dive-average-score": diveData[i][5],
+            "dive-last-performed": diveData[i][6],
+            "dive-selector": diveSelector,
+        };
+        for (var key in diveProperties) {
+            var value = diveProperties[key];
+            $td = $("<td></td>", {"class":key}).html(value);
+            $newDiveRow.append($td);
         }
-
+        $("#dive-database-table").append($newDiveRow);
     }
+}
+
+function toggleDive(clicked) {
+    console.log("toggling!");
+    $("#divelist-container").addClass("hide-quicklist");
+    $(clicked).toggleClass("selected");
+    if ($(clicked).hasClass("selected")) { // Add it to the box
+        $("#list-view").append('<span class="selected-dive" id="'+clicked.id+'_selected">'+clicked.id+' &nbsp; <strong>Dive</strong></span>');
+    } else { // remove it from the box
+        $('#'+clicked.id+'_selected').remove();
+    }
+}
+
+$(document).ready(function() {
+    
+//    loadDiveData("all_dives.csv");
+    loadDiveData("dive_data.csv");
+    
+    // Bind Action Listeners
+    $(".dive-entry").click(toggleDive);
 });
 
 
